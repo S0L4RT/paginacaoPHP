1a6b936c526881c319448a9870752d2d5bba38ef
Arthur Maciel <134110407+S0L4RT@users.noreply.github.com>
2024-06-17 17:58:48 -0300

    Update index.php

---
+++ home/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Exercícios PHP</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2560px-PHP-logo.svg.png" alt="phpLogo">
+    </header>
+    <main>
+        <li><a id="primeiro" class="button-72" role="button" href="rifa/rifa.php">Rifa Digital</a></li>
+        <li><a class="button-72" role="button" href="imc/imc.php">Calcular IMC</a></li>
+        <li><a class="button-72" role="button" href="gorjeta/gorjeta.php">Calcular gorjeta</a></li>
+        <li><a class="button-72" role="button" href="convTemp/convTemp.php">Conversão de temperatura</a></li>
+        <li><a class="button-72" role="button" href="convMoeda/convCoin.php">Conversão de moeda</a></li>
+        <li><a class="button-72" role="button" href="calArea/formas.php">Calculadora de áreas</a></li>
+    </main>
+    <footer>
+        Copyright Arthur Maciel da Rosa
+    </footer>
+</body>
+</html>
